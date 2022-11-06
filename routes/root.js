c041f83553f3f6387094d489f29168de6ac0670a
Ayotunde Pedro <ayopedro2012@gmail.com>
2022-11-06 13:38:40 +0100

    Update home route

--- routes/root.js
+++ routes/root.js
@@ -1,9 +1,13 @@
-const {Router} = require('express')
-const path = require('path')
-const router = Router()
+const { Router } = require("express");
+const path = require("path");
+const router = Router();
 
-router.get('/', (req, res) => {
-    res.send(path.join(__dirname, 'README.md'))
-})
+router.get("/", (req, res) => {
+  res.send(`
+    <h1>Welcome to Blurga</h1>
+    <p>A blogging API - An AltSchool Assessment Project</p>
+    <p>For Details, visit the <a href="https://github.com/ayopedro/blurga">repository</a></p>
+    `);
+});
 
 module.exports = router;
